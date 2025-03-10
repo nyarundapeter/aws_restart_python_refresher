3f6c1f300edaf3cf2f99706a5aaa58132ae161ab
Peter Nyarunda <ec2-user@ip-10-16-10-84.us-west-2.compute.internal>
2025-03-10 02:11:13 +0000

    feat:Add numeric data types python file
    This file illustrates different types of numeric data types.

---
+++ numeric-data.py
@@ -0,0 +1,38 @@
+"""
+Python Numeric Data Types
+- Int
+- Float
+- Complex Number
+- Boolean Values
+"""
+print("Python has three numeric types : int, float, and complex")
+
+myValue=1
+
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=3.14
+
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=5j
+
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=True
+
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
